8e7bf2948f1c61d5aa952c1c74ab18a19d672f4c
tarik <tarik@34f69392-1f83-4d60-987b-fdb6d18ce014>
2009-09-09 15:05:08 +0000

    added Google Analytics code

--- application/tpl/bas.php
+++ application/tpl/bas.php
@@ -3,5 +3,14 @@
 			NoteTonProf.com est un site de <a href="http://www.campuscitizens.com">Campus Citizens</a> - Tous droits réservés. - <a href="dessin">Dessin</a>
 			</p>
 	</div>
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-10592783-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>
 </body>
 </html>
