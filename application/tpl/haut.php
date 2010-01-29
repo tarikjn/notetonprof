7d2368c95998d15eca11824283cfc1e3972b97a4
tarik <tarik@34f69392-1f83-4d60-987b-fdb6d18ce014>
2010-01-29 00:43:18 +0000

    added left bottom ads

--- application/tpl/haut.php
+++ application/tpl/haut.php
@@ -128,6 +128,21 @@ $map_haut = array(
 				}
 			?>
 		</div>
+		
+		<div class="ad-panel">
+			<script type="text/javascript"><!--
+			google_ad_client = "pub-5461332511807874";
+			/* 200x200, created 1/28/10 */
+			google_ad_slot = "0990054607";
+			google_ad_width = 200;
+			google_ad_height = 200;
+			//-->
+			</script>
+			<script type="text/javascript"
+			src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+			</script>
+		</div>
+		
 	</div>
 <? if (@$show_stats) require("tpl/stats-droite.php"); ?>
 	<div class="<?=(@$show_stats)?"centre":"page"?>">
